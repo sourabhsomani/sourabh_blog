dc3a1d3d39b606b7d8f6f5cfd5d9cd4f5361b0ce
Sourabh Somani <sourabh_somani2010@hotmail.com>
2019-03-29 18:00:36 +0530

    Service Worker Bug Fixes

--- service-worker.js
+++ service-worker.js
@@ -37,16 +37,18 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    cache.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    }).catch(function() {
-      // If it can't fetch the asset, display the offline only page
-      return caches.match('/offline.html')
-    })
-  );
+ self.addEventListener('fetch', function(event) {
+  // We only want to call event.respondWith() if this is a GET request for an HTML document.
+  if (event.request.method === 'GET' &&
+      event.request.headers.get('accept').indexOf('text/html') !== -1) {
+    console.log('Handling fetch event for', event.request.url);
+    event.respondWith(
+      fetch(event.request).catch(function(e) {
+        console.error('Fetch failed; returning offline page instead.', e);
+        return caches.open(OFFLINE_CACHE).then(function(cache) {
+          return cache.match('/offline.html');
+        });
+      })
+    );
+  }
 });
